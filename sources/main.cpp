6ed7cb82975148775e4137c430fb9631051607db
porcorosso56 <47530066+porcorosso56@users.noreply.github.com>
2019-12-16 17:36:16 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -13,22 +13,29 @@ int main() {
     ifstream in(path1);
     json obj = json::parse(in);
     in.close();
-    auto arr1 = obj[0];
-    auto arr2 = obj[1];
-    auto arr3 = obj[2];
-    std::ofstream o("file2.json");
-    o << "[\n";
-    for(int i = 0; i < 3; i++) {
-	o << "    "  << "{ \"ticker\" : " << arr1[i] << ", \"id\": " << arr2[i] <<", \"description\": " << arr3[i] << "}";
-        if (i != 2) o << ",";
-	o << "\n";
+    if (obj.size() == 3)
+    {
+	    auto arr1 = obj[0];
+	    auto arr2 = obj[1];
+	    auto arr3 = obj[2];
+	    std::ofstream o("file2.json");
+	    o << "[\n";
+	    for(int i = 0; i < 3; i++) {
+		o << "    "  << "{ \"ticker\" : " << arr1[i] << ", \"id\": " << arr2[i] <<", \"description\": " << arr3[i] << "}";
+		if (i != 2) o << ",";
+		o << "\n";
+	    }
+	    o << "]\n";
+	    o.close();
+	    ifstream in1(path2);
+	    json new_obj;
+	    in1 >> new_obj;
+	    in1.close();
+	    cout << new_obj.dump(5);
+	    return 0;
     }
-    o << "]\n";
-    o.close();
-    ifstream in1(path2);
-    json new_obj;
-    in1 >> new_obj;
-    in1.close();
-    cout << new_obj.dump(5);
-    return 0;
+    else 
+    {
+	    std::cout << "Data is not valid!\n";
+    }
 }
